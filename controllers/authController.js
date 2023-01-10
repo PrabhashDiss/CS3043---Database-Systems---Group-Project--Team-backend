54f8332ef1ab6503691085452c88d5a5d89cf34a
Movin Silva <movinpinsara@gmail.com>
2023-01-10 13:09:12 +0530

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,39 +1,43 @@
 const jwt = require("jsonwebtoken");
+const { query } = require("../database/dbConnect");
 
 const login_post = (req, res, next) => {
-  const users = query(
+  query(
     `select user,password,role
      from users
-     where user = '${req.body.user_is}' and password = '${req.body.password}';`)
+     where user = '${req.body.user_id}' and password = '${req.body.password}';`
+  ).then((users) => {
+    const { user_id, password } = req.body;
 
-  const { user, password } = req.body;
+    const User = users.find(
+      (u) => u.rows[0].user === user_id && u.rows[0].password === password
+    );
 
-  const User = users.find((u) => u.rows[0].user === user && u.rows[0].password === password);
-
-  if (!User) {
-    return res.status(401).send({ message: "Invalid email or password" });
-  }
-
-  const token = jwt.sign(
-    { user: data.rows[0].user, role: data.rows[0].role },
-    process.env.SECRET_KEY,
-    {
-      expiresIn: "1800s",
+    if (!User) {
+      return res.status(401).send({ message: "Invalid email or password" });
     }
-  );
 
-  return res.status(200).send({success: true, token : token})
-}
+    const token = jwt.sign(
+      { user: data.rows[0].user, role: data.rows[0].role },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1800s",
+      }
+    );
+
+    return res.status(200).send({ success: true, token: token });
+  });
+};
 
 const customer_register_post = (req, res, next) => {
   const data = query(
-    `insert into users values ('${req.body.user}','${req.body.password}','${req.body.role}');`)
-    .then((rows) => {
-         return res.send(rows)
-    })
-}
+    `insert into users values ('${req.body.user}','${req.body.password}','${req.body.role}');`
+  ).then((rows) => {
+    return res.send(rows);
+  });
+};
 
 module.exports = {
   login_post,
-  customer_register_post
+  customer_register_post,
 };
