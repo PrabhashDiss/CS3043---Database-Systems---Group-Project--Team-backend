b492c7f1376238148c309a781352915517030cb2
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-01-10 17:07:39 +0530

    Update commonController.js

--- controllers/commonController.js
+++ controllers/commonController.js
@@ -9,6 +9,7 @@ const get_branch = (req, res, next) => {
 }
 
 const add_loan = (req, res, next) => {
+    /*
     let loan = req.body;
     var sql = "SET @LoanNumber = ?;SET @BranchCode = ?;SET @LoanTypeID = ?;SET @LoanDuration = ?;SET @InterestRate = ?;SET @StartDate = ?;SET @DueDate = ?;SET @IsPersonal = ?;SET @IsOnline = ?; \
     CALL LoanAdd(@LoanNumber,@BranchCode,@LoanTypeID,@LoanDuration,@InterestRate,@StartDate,@DueDate,@IsPersonal,@IsOnline);"
@@ -16,6 +17,13 @@ const add_loan = (req, res, next) => {
         sql,[loan.loan_number, loan.branch_code, loan.loan_type_id, loan.loan_duration, loan.interest_rate, loan.start_date, loan.due_date, loan.is_personal, loan.is_online])
         .then((rows) => {
              return res.send(rows)
+        })
+    */
+    const data = query(
+        `INSERT INTO loan(loan_number, branch_code, amount, loan_type_id, loan_duration, interest_rate, start_date, due_date, is_personal, is_online, loan_status)
+         VALUES('${req.body.loan_number}', '${req.body.branch_code}', ${req.body.amount}, '${req.body.loan_type_id}', ${req.body.loan_duration}, ${req.body.interest_rate}, '${req.body.start_date}', '${req.body.due_date}', ${req.body.is_personal}, ${req.body.is_online}, ${req.body.loan_status});`)
+        .then((rows) => {
+                return res.send(rows)
         })
 }
 
