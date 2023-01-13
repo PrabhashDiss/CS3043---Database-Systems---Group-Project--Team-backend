716b4c184f6eb146a831391a9e6a673ebe4b0602
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-01-13 16:09:47 +0530

    Update managerController.js

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -87,6 +87,20 @@ const get_loan_to_be_approved = (req, res, next) => {
         })
 }
 
+const get_loan_payment_due_manager = (req, res, next) => {
+    const data = query(`select customer_name, last_payment_date, start_date, ot.customer_id, loan_number, count(loan_number) as payment_count, loan_duration, diff, branch_city
+    from (select customer_name, last_payment_date, start_date, it.customer_id, loan_number, count(loan_number), loan_duration, diff, branch_code
+        from (select branch_code, DATE_ADD(start_date, INTERVAL loan_duration - count(loan_number) MONTH) as last_payment_date, customer_id, start_date, loan_number, count(loan_number), loan_duration, loan_duration - count(loan_number) as diff
+            from loan_payment left outer join (select branch_code, loan_number,loan_duration, start_date, customer_id
+                                            from loan natural join (select loan_number, customer_id
+                                                                from account natural join loan_account) as j) as k
+                                                                using (loan_number) 
+        group by loan_number) as it inner join customer on customer.customer_id = it.customer_id) as ot inner join branch on ot.branch_code = branch.branch_code`)
+        .then((rows) => {
+            return res.send(rows)
+        })
+}
+
 module.exports = {
-    get_employee_branch, get_fd, get_employee, add_employee, approve_loan, get_loan_to_be_approved
+    get_employee_branch, get_fd, get_employee, add_employee, approve_loan, get_loan_to_be_approved, get_loan_payment_due_manager
 }
