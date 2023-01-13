1f58f62cb57e4e7f439d2bd3bb5078a5c27202a3
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-01-13 14:32:09 +0530

    Create formatDate.js

---
+++ helpers/formatDate.js
@@ -0,0 +1,28 @@
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    return h + ":" + m + ":" + s;
+}
+function formatDate() {
+    var d = new Date(), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-') + ' ' + startTime()
+}
+
+module.exports = {
+    formatDate
+}
